faeb2588d7a10f93f5567047a1a014852463f66e
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2017-05-16 23:29:48 +0300

    Added examples for first lesson
    Added examples for first lesson

--- spec.js
+++ spec.js
@@ -1,5 +1,50 @@
-describe('', ()=> {
-    it('', ()=> {
-        expect(true).toBe(true)
+describe('Test', function () {
+    it('1 test', function () {
+        const URL = 'https://movies-finder.firebaseapp.com/'
+
+        browser.get(URL)
+        browser.sleep(10000)
+        expect(browser.getCurrentUrl()).toEqual(URL);
+        expect(browser.getTitle()).toEqual('Movies Finder')
+    })
+
+    it('2 test', function () {
+        let locator = By.css('input.form-control')
+        let searchField = element(locator)
+
+        const URL = 'https://movies-finder.firebaseapp.com/'
+
+        browser.get(URL)
+        browser.sleep(10000)
+
+        expect(searchField.isPresent()).toBeTruthy()
+        expect(searchField.isDisplayed()).toBeTruthy()
+    })
+
+    it('3 test', function () {
+        let locator = By.css('input.form-control')
+        let searchField = element(locator)
+
+        const URL = 'https://movies-finder.firebaseapp.com/'
+
+        browser.get(URL)
+        browser.sleep(10000) 
+
+        expect(searchField.getAttribute('placeholder')).toEqual('Search for movies...')
+    })
+
+    it('4 test', function () {
+        let locator = By.css('input.form-control')
+        let searchField = element(locator)
+
+        const URL = 'https://movies-finder.firebaseapp.com/'
+
+        browser.get(URL)
+        browser.sleep(3000) 
+
+        searchField.sendKeys('HELLO WORLD')
+        browser.sleep(10000)
+        element(By.css('span.input-group-btn button.btn.btn-primary')).click()
+        browser.sleep(10000)
     })
 })
